ef9a2473d16161ce7bc3a5eae6e9305588046f54
HS <henrik.svensson@yaelev.se>
2016-11-02 10:56:06 +0100

    Fixade Mainform

--- OOLabb2_Grumpy/OOLabb2_Grumpy/Program.cs
+++ OOLabb2_Grumpy/OOLabb2_Grumpy/Program.cs
@@ -16,7 +16,7 @@ namespace OOLabb2_Grumpy
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
