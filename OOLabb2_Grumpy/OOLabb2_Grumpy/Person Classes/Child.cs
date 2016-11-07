88d9e866085daf4ffa47559ef582f79a78003d74
HS <henrik.svensson@yaelev.se>
2016-11-07 10:00:47 +0100

    fixade child's ToString

--- OOLabb2_Grumpy/OOLabb2_Grumpy/Person Classes/Child.cs
+++ OOLabb2_Grumpy/OOLabb2_Grumpy/Person Classes/Child.cs
@@ -8,10 +8,17 @@ namespace OOLabb2_Grumpy
 {
     public class Child : Person 
     {
+        private string title = "Child.";
+
         public Child(string fName, string lName)
             : base (fName, lName)
         {
 
+        }
+
+        public override string ToString()
+        {
+            return title + " " + firstName + " " + lastName;
         }
     }
 }
