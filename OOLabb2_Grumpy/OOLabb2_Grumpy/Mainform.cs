69e71eb8608c3b2fdc6f5d9289793fd76506e06e
HS <henrik.svensson@yaelev.se>
2016-11-07 10:22:35 +0100

    Städade kod grundligt

--- OOLabb2_Grumpy/OOLabb2_Grumpy/Mainform.cs
+++ OOLabb2_Grumpy/OOLabb2_Grumpy/Mainform.cs
@@ -12,6 +12,8 @@ namespace OOLabb2_Grumpy
 {
     public partial class MainForm : Form
     {
+        System.Media.SoundPlayer scream = new System.Media.SoundPlayer("scream.wav");
+
         //Bool's to ensure the stability of this program. All hail the bool.
         private bool firstnameAcceptable, lastnameAcceptable, genderSelected = false;
 
@@ -39,7 +41,7 @@ namespace OOLabb2_Grumpy
         //    personList.Add(new Child())
         //}
 
-        //Checks the creation textboxes, making sure they're not empty. If empty, the "create human"-button will be disabled.
+        //Checks the creation textboxes, making sure they're not empty. If empty, the appropriate button will be disabled.
         private void firstnameTextbox_TextChanged(object sender, EventArgs e)
         {
             if (firstnameTextbox.Text != "")
@@ -80,6 +82,31 @@ namespace OOLabb2_Grumpy
                 createButton.Enabled = false;
             }
         }
+        private void textSearch_TextChanged(object sender, EventArgs e)
+        {
+            if (searchTextbox.Text == "")
+            {
+                buttonClear.Enabled = false;
+            }
+            else
+            {
+                buttonClear.Enabled = true;
+            }
+            string tmp = searchTextbox.Text;
+            personListbox.Items.Clear();
+            foreach (var item in personList)
+            {
+                if (item.firstName.ToLower().Contains(tmp.ToLower()))
+                {
+                    personListbox.Items.Add(item);
+                }
+                else if (item.lastName.ToLower().Contains(tmp.ToLower()))
+                {
+                    personListbox.Items.Add(item);
+                }
+            }
+
+        }
         //These just make sure that a radio button has been selected.
         private void manRadio_CheckedChanged(object sender, EventArgs e)
         {
@@ -106,6 +133,23 @@ namespace OOLabb2_Grumpy
             }
         }
 
+        //Adds a man. Or a woman.
+        private void createButton_Click(object sender, EventArgs e)
+        {
+
+            if (manRadio.Checked)
+            {
+                personList.Add(new Male(firstnameTextbox.Text, lastnameTextbox.Text));
+            }
+            else
+            {
+                personList.Add(new Female(firstnameTextbox.Text, lastnameTextbox.Text));
+            }
+
+            ReList();
+
+        }
+
         //Removes people.
         //Whether they want to or not..
         private void removeButton_Click(object sender, EventArgs e)
@@ -114,40 +158,30 @@ namespace OOLabb2_Grumpy
             {
                 personList.Remove((Person)p);
             }
+            scream.Play();
             ReList();
         }
 
-        private void textSearch_TextChanged(object sender, EventArgs e)
-        {
-            if (searchTextbox.Text == "")
-            {
-                buttonClear.Enabled = false;
-            }
-            else
-            {
-                buttonClear.Enabled = true;
-            }
-            string tmp = searchTextbox.Text;
-            personListbox.Items.Clear();
-            foreach (var item in personList)
-            {
-                if (item.firstName.ToLower().Contains(tmp.ToLower()))
-                {
-                    personListbox.Items.Add(item);
-                }
-                else if (item.lastName.ToLower().Contains(tmp.ToLower()))
-                {
-                    personListbox.Items.Add(item);
-                }
-            }
-
-        }
-
+        //Clears the searchbox
         private void buttonClear_Click(object sender, EventArgs e)
         {
             searchTextbox.Text = "";
         }
+        //Sorts the list.
+        private void sortButton_Click(object sender, EventArgs e)
+        {
+            personListbox.Items.Clear();
+            // Uses IComparable.CompareTo()
+            personList.Sort();
 
+            // Uses Employee.ToString
+            foreach (var item in personList)
+            {
+                personListbox.Items.Add(item);
+            }
+        }
+
+        //Adds some victi... I mean volunteers for testing the program.
         private void buttonAddSomePeople_Click(object sender, EventArgs e)
         {
             //Add some people to the empty listbox
@@ -167,19 +201,7 @@ namespace OOLabb2_Grumpy
 
         }
 
-        private void sortButton_Click(object sender, EventArgs e)
-        {
-            personListbox.Items.Clear();
-            // Uses IComparable.CompareTo()
-            personList.Sort();
-
-            // Uses Employee.ToString
-            foreach (var item in personList)
-            {
-                personListbox.Items.Add(item);
-            }
-        }
-
+        //Creates a child. Birds and bees you know?
         private void mergeButton_Click(object sender, EventArgs e)
         {
 
@@ -189,20 +211,6 @@ namespace OOLabb2_Grumpy
         {
 
         }
-        private void createButton_Click(object sender, EventArgs e)
-        {
-            
-            if (manRadio.Checked)
-            {
-                personList.Add(new Male (firstnameTextbox.Text, lastnameTextbox.Text));
-            }
-            else
-            {
-                personList.Add(new Female(firstnameTextbox.Text, lastnameTextbox.Text));
-            }
-
-            ReList();
-
-        }
+        
     }
 }
